5aa2a74894c4011d39cfe60748503edbdeaa94c8
hsuweiwei <qqq987410@gmail.com>
2020-11-20 12:02:00 +0800

    init

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.scss";
+
+function App() {
+   return (
+      <div>
+         <h1 className={styles.demo}>Shit</h1>
+      </div>
+   );
+}
+
+export default App;
